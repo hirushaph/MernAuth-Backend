897033e388ef505df5043b527da449abfae5033e
Hirusha Prabhanath <hirushaph@gmail.com>
2024-01-24 13:49:47 +0530

    update login endpoint

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const UserModel = require("../models/userModel");
 
 // bcrypt salt rounds
@@ -49,7 +50,42 @@ async function registerUser(req, res) {
 }
 
 async function login(req, res) {
-  res.json("login");
+  try {
+    const { username, password } = req.body;
+
+    if (!username) return res.send({ error: "Username is required" });
+    if (!password) return res.send({ error: "Password is required" });
+
+    const user = await UserModel.findOne({ username });
+    console.log(user);
+
+    if (!user) return res.status(404).send({ error: "Username not found" });
+
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (err) return res.status(500).send({ error: "Internal Server Error" });
+
+      if (!result) return res.status(400).send({ error: "Wrong Password" });
+
+      const token = jwt.sign(
+        {
+          userId: user._id,
+          username: user.username,
+        },
+        process.env.JWT_SKEY,
+        {
+          expiresIn: "24h",
+        }
+      );
+
+      return res.status(200).send({
+        msg: "Login Successfull",
+        username: user.username,
+        token,
+      });
+    });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 }
 
 async function getUser(req, res) {
