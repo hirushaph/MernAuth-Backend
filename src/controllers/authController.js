9ff654c88fab55615ecd488c08c732b4be1a4fd1
Hirusha Prabhanath <hirushaph@gmail.com>
2024-02-03 23:18:44 +0530

    Revert "Remove necessary items from register response"
    This reverts commit cbb7dd4c22183e0927c1a6ee0128f4ef93d03248.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -33,12 +33,12 @@ async function registerUser(req, res) {
     const usernameExits = await UserModel.exists({ username });
     const emailExits = await UserModel.exists({ email });
     if (emailExits || usernameExits) {
-      let errors = [];
+      let erros = [];
 
-      if (usernameExits) errors.push("Username unavaliable");
-      if (emailExits) errors.push("Email already in use");
+      if (usernameExits) erros.push("Username unavaliable");
+      if (emailExits) erros.push("Email already in use");
 
-      return res.status(400).json({ errors });
+      return res.status(400).json({ erros });
     }
 
     // Hash password and Store in db
@@ -70,7 +70,7 @@ async function registerUser(req, res) {
 
           res
             .status(201)
-            .send({ username: user.username, token, refreshToken });
+            .send({ msg: "User registerd successfully", token, refreshToken });
         })
         .catch((err) => res.status(500).send(err));
     });
